dd2e1c12d79354c7c6878312adde7b6a51574ae1
Jimmyquinn4 <40280727+Jimmyquinn4@users.noreply.github.com>
2019-06-18 13:40:48 -0400

    Create diskuse.py

---
+++ diskuse.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+
+import os,psutil,smtplib,socket
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+
+THRESHOLD = 95
+partitions = psutil.disk_partitions(all=False)
+message = 'WARNING: DISK(S) OVER ' + str(THRESHOLD) + '% FULL'
+diskname = []
+diskpercent = []
+email_receiver = "jamesqui@wharton.upenn.edu"
+
+#for loop to iterate through disks and monitor usage
+for p in partitions:
+        diskuse = (psutil.disk_usage(p.mountpoint).percent)
+        if diskuse >= THRESHOLD:
+                diskname.append(p.mountpoint)
+                diskpercent.append(psutil.disk_usage(p.mountpoint).percent)
+
+#if statement to email the disk name and percentage used
+if diskuse >= THRESHOLD:
+        msg = MIMEText(message + ' ' + (str(dict(zip(diskname,diskpercent)))))
+        msg["To"] = email_receiver
+        msg["Subject"] = '[' + (socket.gethostname()) + '] ' +  "DISK OVER THRESHOLD"
+        P = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE, universal_newlines=True)
+        P.communicate(msg.as_string())
